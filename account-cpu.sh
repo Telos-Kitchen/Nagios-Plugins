a54722d2b527ad83d8ce254947959b4e9dcddc3d
Max <max@digitalscarcity.io>
2019-09-01 11:37:16 -0400

    check CPU usage

--- account-cpu.sh
+++ account-cpu.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 DIR=/opt
-CLEOS=$DIR/cleos.sh
+CLEOS=cleos
 
 #Set max CPU average to compare against
 MAX=30
 
-#Create blank file
 echo -n >CPUSTATS
 
 
 end=$((SECONDS+60))
 while [ $SECONDS -lt $end ]; do   # Loop until interval has elapsed.
 
-        CPUA=$($CLEOS get account chintailease -j | jq -r ".cpu_limit.available")
-        CPUU=$($CLEOS get account chintailease -j | jq -r ".cpu_limit.used")
+        CPUA=$($CLEOS get account teloskitchen -j | jq -r ".cpu_limit.available")
+        CPUU=$($CLEOS get account teloskitchen -j | jq -r ".cpu_limit.used")
         CPUR="(($CPUU/$CPUA)*100)"
         CPURA=$(echo "scale=4; ${CPUR}" | bc | cut -d'.' -f 1 | sed 's/ //g')
         # Append each line to CPUSTATS file
@@ -26,9 +25,9 @@ AVG=$(awk '{s+=$1} END {print s/NR}' CPUSTATS)
 
 
 if (( $(echo "$AVG > $MAX" |bc -l) )); then
-  echo "Chintai CPU is above $MAX usage, currently averaging $AVG usage | Chintaileasecpu=$AVG "
+  echo "teloskitchen CPU is above $MAX usage, currently averaging $AVG usage | teloskitchencpu=$AVG "
   exit 2
 else
-  echo "Chintai CPU is below $MAX usage, currently averaging $AVG usage | Chintaileasecpu=$AVG"
+  echo "teloskitchen CPU is below $MAX usage, currently averaging $AVG usage | teloskitchencpu=$AVG"
   exit 0
 fi
